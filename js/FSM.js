04026664bcb13639a966262496001257bcd9ec1f
Siemen Baader <siemenbaader@gmail.com>
2013-12-18 21:46:42 +0100

    initial

---
+++ js/FSM.js
@@ -0,0 +1,7 @@
+define("fsm/FSM", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_core/Kernel-Objects"], function(smalltalk,nil,_st){
+smalltalk.addPackage('FSM');
+smalltalk.packages["FSM"].transport = {"type":"amd","amdNamespace":"fsm"};
+
+smalltalk.addClass('FSM', smalltalk.Object, [], 'FSM');
+
+});
